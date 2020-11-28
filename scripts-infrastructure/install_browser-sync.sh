9f9dc8c4b7f4334ab67ae8e2c790218676b41951
BrouKe <keithgb@github.com>
2020-11-27 20:55:54 -0600

    initial development push

---
+++ scripts-infrastructure/install_browser-sync.sh
@@ -0,0 +1,6 @@
+#!bin/sh
+
+sudo npm i -g browser-sync
+
+#i.e.
+browser-sync start --server --files "css/*.css"
---
+++ scripts-infrastructure/install_validate_html.sh
@@ -0,0 +1,6 @@
+#!bin/sh
+
+sudo npm i -g html-validate
+
+#i.e.
+html-validate "../wwwroot/**/*.html"
